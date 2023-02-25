b733275b75970e53a34170c5f3ab9dce55bb7b5c
AkashChakrabortty <akashchakrabortty2000@gmail.com>
2023-02-25 20:52:11 +0600

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import Box from '@mui/material/Box';
 const Footer = () => {
     return (
         <div>
-            <h1>Footer</h1>
+          <Box sx={{ p: 2, textAlign: 'center', fontSize:'20px'}}>
+          <p className='default-font'>Copyright © 2023 - All right reserved by Akash Chakrabortty</p>
+         </Box>
         </div>
     );
 };
