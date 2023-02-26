e1ac4e52ea276db760448f97d147127b49086b1d
AkashChakrabortty <akashchakrabortty2000@gmail.com>
2023-02-27 02:06:46 +0600

    a

--- src/components/KnowMeMore.js
+++ src/components/KnowMeMore.js
@@ -3,7 +3,9 @@ import React from 'react';
 const KnowMeMore = () => {
     return (
         <div>
-            <h1>Know Me More</h1>
+           <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
         </div>
     );
 };
